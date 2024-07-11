de546c76be281c9684bb2efce59b789fc7302c41
codedreammer <nkanal38@gmail.com>
2024-07-11 18:13:53 +0530

    add the function

--- function.py
+++ function.py
@@ -1,24 +1,27 @@
-def calculateGmean(a, b):
+def calculateGmean(a , b):
     mean = (a*b)/(a+b)
     print(mean)
+
+def isGreater(a,b):
+    if(a>b):
+      print("first no. is greater")
+    else:
+       print("second number is greater or equal")
 
 
 a = 9 
 b = 8
-if(a>b):
-    print("first number is greater")
-else:
-    print("second number is greater or equal")
+isGreater(a,b)
 
-    
-c = 12
-d = 10
-if(c<d):
-    print("first number is smaller")
-else:
-    print("number is smaller then the first")
+calculateGmean(a,b)
 
+#gmean = (a*b)/(a+b)
+#print(gmean1)
 c = 8
-d = 74
-isGreater(c, d)
+d = 7
+
+isGreater(c,d)
+#gmean2 = (c*d)/(c+d)
+#calculateGmean(print(gmean2)
 calculateGmean(c,d)
+
