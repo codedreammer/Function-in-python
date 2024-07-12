6aa9ce4d406ce192d81906501817100ec5980cbf
codedreammer <nkanal38@gmail.com>
2024-07-12 18:06:52 +0530

    add the file

---
+++ arguments .py
@@ -0,0 +1,10 @@
+def average(a = 9, b=1):
+    print("The average iss",(a+b)/2)
+
+
+average()
+def name(fname, mname = "jhon",lname = "whatson"):
+    print("hello",fname,mname,lname)
+
+name("amy","agarwal","jain")
+
