29f0cf6b67d8f63e742172d52c6a169ffabab618
codedreammer <nkanal38@gmail.com>
2024-07-12 18:17:52 +0530

    add more

--- arguments .py
+++ arguments .py
@@ -1,10 +1,19 @@
-def average(a = 9, b=1):
-    print("The average iss",(a+b)/2)
+#def average(a = 9, b=1):
+ #   print("The average iss",(a+b)/2)
 
 
-average()
-def name(fname, mname = "jhon",lname = "whatson"):
-    print("hello",fname,mname,lname)
+#average()
+#def name(fname, mname = "jhon",lname = "whatson"):
+ #   print("hello",fname,mname,lname)
+#
+#name("amy","agarwal","jain")
 
-name("amy","agarwal","jain")
+def average(*numbers):
+    sum = 0
+    for i in numbers:
+        sum = sum + i 
+        print("Average is:",sum/len(numbers))
+    
+    average(5, 6, 7, 1)
+
 
